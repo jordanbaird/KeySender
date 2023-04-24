b563883d07d56a61c4e653bcd35282be5d8be633
Jordan Baird <jordanbaird.dev@gmail.com>
2023-04-24 11:18:53 -0600

    Convert `KeyEvent` to class

--- Sources/KeySender/KeyEvent.swift
+++ Sources/KeySender/KeyEvent.swift
@@ -5,11 +5,27 @@
 
 import Cocoa
 
-/// A representation of a key event that can be sent by an
-/// instance of ``KeySender/KeySender``.
-public struct KeyEvent {
+/// A key event that can be sent by a key sender.
+public class KeyEvent {
 
     // MARK: Properties
+
+    private lazy var _cgEvent: CGEvent? = {
+        let cgEvent = CGEvent(
+            keyboardEventSource: CGEventSource(stateID: .hidSystemState),
+            virtualKey: CGKeyCode(key.rawValue),
+            keyDown: kind == .keyDown
+        )
+        cgEvent?.flags = modifiers.flags
+        return cgEvent
+    }()
+
+    private lazy var _nsEvent: NSEvent? = {
+        guard let _cgEvent else {
+            return nil
+        }
+        return NSEvent(cgEvent: _cgEvent)
+    }()
 
     /// The key associated with this key event.
     public let key: Key
@@ -19,6 +35,12 @@ public struct KeyEvent {
 
     /// The kind of this key event.
     public let kind: EventKind
+
+    /// The Core Graphics event corresponding to this key event.
+    public var cgEvent: CGEvent? { _cgEvent }
+
+    /// The Cocoa event corresponding to this key event.
+    public var nsEvent: NSEvent? { _nsEvent }
 
     // MARK: Initializers
 
@@ -30,7 +52,7 @@ public struct KeyEvent {
     }
 
     /// Creates a key event from the given Core Graphics event.
-    public init?(cgEvent: CGEvent) {
+    public convenience init?(cgEvent: CGEvent) {
         guard
             let key = Key(rawValue: Int(cgEvent.getIntegerValueField(.keyboardEventKeycode))),
             let kind = EventKind(cgEventType: cgEvent.type)
@@ -39,40 +61,58 @@ public struct KeyEvent {
         }
         let modifiers = Modifiers(flags: cgEvent.flags)
         self.init(key: key, modifiers: modifiers, kind: kind)
+        self._cgEvent = cgEvent
     }
 
     /// Creates a key event from the given Cocoa event.
-    public init?(nsEvent: NSEvent) {
+    public convenience init?(nsEvent: NSEvent) {
         guard let cgEvent = nsEvent.cgEvent else {
             return nil
         }
         self.init(cgEvent: cgEvent)
+        self._nsEvent = nsEvent
     }
-}
 
-extension KeyEvent {
-    /// The Core Graphics event corresponding to this key event.
-    public var cgEvent: CGEvent? {
-        let event = CGEvent(
-            keyboardEventSource: CGEventSource(stateID: .hidSystemState),
-            virtualKey: CGKeyCode(key.rawValue),
-            keyDown: kind == .keyDown
+    public required convenience init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        try self.init(
+            key: container.decode(Key.self, forKey: .key),
+            modifiers: container.decode(Modifiers.self, forKey: .modifiers),
+            kind: container.decode(EventKind.self, forKey: .kind)
         )
-        event?.flags = modifiers.flags
-        return event
-    }
-
-    /// The Cocoa event corresponding to this key event.
-    public var nsEvent: NSEvent? {
-        cgEvent.flatMap { NSEvent(cgEvent: $0) }
     }
 }
 
 // MARK: KeyEvent: Codable
-extension KeyEvent: Codable { }
+extension KeyEvent: Codable {
+    private enum CodingKeys: CodingKey {
+        case key
+        case modifiers
+        case kind
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(key, forKey: .key)
+        try container.encode(modifiers, forKey: .modifiers)
+        try container.encode(kind, forKey: .kind)
+    }
+}
 
 // MARK: KeyEvent: Equatable
-extension KeyEvent: Equatable { }
+extension KeyEvent: Equatable {
+    public static func == (lhs: KeyEvent, rhs: KeyEvent) -> Bool {
+        lhs.key == rhs.key &&
+        lhs.modifiers == rhs.modifiers &&
+        lhs.kind == rhs.kind
+    }
+}
 
 // MARK: KeyEvent: Hashable
-extension KeyEvent: Hashable { }
+extension KeyEvent: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(key)
+        hasher.combine(modifiers)
+        hasher.combine(kind)
+    }
+}
